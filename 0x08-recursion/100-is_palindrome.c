ba6856a37b72c1162a4111719bd64ac260fbec00
mohamed-ifkirn <moifkirn1@gmail.com>
2023-04-05 18:13:38 -0700

    100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,39 +1,39 @@
 #include "main.h"
 /**
  * _strlen_recursion - Prints the length of a string.
- * @a: the string to be printed
+ * @s: the string to be printed
  * Return: the length of string
  */
-int _strlen_recursion(char *a)
+int _strlen_recursion(char *s)
 {
-if (a[0] != '\0')
-return (1 + _strlen_recursion(a + 1));
+if (s[0] != '\0')
+return (1 + _strlen_recursion(s + 1));
 return (0);
 }
 /**
  * pal_checker - check if s is palindrome.
- * @a: string base address.
+ * @s: string base address.
  * @i: left index.
- * @d: rigth index.
+ * @j: rigth index.
  * Return: 1 if s is palindrome, 0 otherwise.
  */
-int pal_checker(char *a, int i, int d)
+int pal_checker(char *s, int i, int j)
 {
-if (s[i] == s[d])
-if (i > d / 2)
+if (s[i] == s[j])
+if (i > j / 2)
 return (1);
 else
-return (pal_checker(a, i + 1, d - 1));
+return (pal_checker(s, i + 1, j - 1));
 else
 return (0);
 }
 /**
  * is_palindrome - check if s is palindrome
- * @a: base address for string.
+ * @s: base address for string.
  *
  * Return: 1 if n is prime, 0 otherwise.
  */
-int is_palindrome(char *a)
+int is_palindrome(char *s)
 {
-return (pal_checker(a, 0, _strlen_recursion(a) - 1));
+return (pal_checker(s, 0, _strlen_recursion(s) - 1));
 }
