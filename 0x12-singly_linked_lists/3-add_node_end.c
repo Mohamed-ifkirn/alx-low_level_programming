02c156a1d2799e996ff3e78a0284567024d1af1d
mohamed-ifkirn <moifkirn1@gmail.com>
2023-04-27 16:57:03 -0700

    3-add_node_end.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,66 +1,54 @@
-#include <stdlib.h>
-#include <string.h>
 #include "lists.h"
+#include <string.h>
 
 /**
- * _strlen - finds the length of a string
- * @str: string to find the length of
+ * add_node_end - adds a new node at the end of a list_t list.
+ * @head: pointer to the head of list_t list
+ * @str: string to be duplicated
  *
- * Return: length of string
- */
-unsigned int _strlen(char *str)
-{
-    unsigned int i;
-
-    for (i = 0; str[i]; i++)
-        ;
-    return (i);
-}
-
-/**
- * add_node_end - adds a new node to the end of linked list
- * @head: double pointer to a linked list
- * @str: string to add to the new node
+ * Return: the address of the new element, or NULL if it failed.
  *
- * Return: pointer to the new node
  */
+
 list_t *add_node_end(list_t **head, const char *str)
 {
-    list_t *new_node, *current_node;
+char *dup;
+int len;
+list_t *new_node, *last_node;
 
-    if (str == NULL)
-        return (NULL);
+new_node = (list_t *) malloc(sizeof(list_t));
+if (new_node == NULL)
+{
+return (NULL);
+}
 
-    new_node = malloc(sizeof(list_t));
+dup = strdup(str);
 
-    if (new_node == NULL)
-        return (NULL);
+if (dup == NULL)
+{
+free(new_node);
+return (NULL);
+}
 
-    new_node->str = strdup(str);
+len = strlen(dup);
 
-    if (new_node->str == NULL)
-    {
-        free(new_node);
-        return (NULL);
-    }
+new_node->str = dup;
+new_node->len = len;
+new_node->next = NULL;
 
-    new_node->len = _strlen(new_node->str);
-    new_node->next = NULL;
+if (*head == NULL)
+{
+*head = new_node;
+}
+else
+{
+last_node = *head;
+while (last_node->next != NULL)
+{
+last_node = last_node->next;
+}
+last_node->next = new_node;
+}
 
-    if (*head == NULL)
-    {
-        *head = new_node;
-        return (new_node);
-    }
-
-    current_node = *head;
-
-    while (current_node->next != NULL)
-    {
-        current_node = current_node->next;
-    }
-
-    current_node->next = new_node;
-
-    return (new_node);
+return (*head);
 }
