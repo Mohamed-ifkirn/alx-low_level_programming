50a7e779d9107c7d6c6e4e908d3b0abaaeed7c03
mohamed-ifkirn <moifkirn1@gmail.com>
2023-04-27 17:02:38 -0700

    2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -4,40 +4,40 @@
 
 /**
  * add_node - adds a new node at the beginning of a list_t list
- * @head: a pointer to the head list_t list
- * @str: A string to be duplicated
- * Return: Address of the new element, or NULL if it failed
+ * @head: a pointer to a pointer to the head of the list
+ * @str: the string to be added to the new node
+ *
+ * Return: a pointer to the new node, or NULL if it fails
  */
 list_t *add_node(list_t **head, const char *str)
 {
-    list_t *new_node = NULL;
-    char *new_str = NULL;
-    size_t str_len = 0;
+	list_t *new_node;
+	char *new_str;
+	unsigned int i;
 
-    if (str == NULL)
-        return NULL;
+	if (head == NULL || str == NULL)
+		return (NULL);
 
-    str_len = strnlen(str, 1024);
-    new_str = malloc(str_len + 1);
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return (NULL);
 
-    if (new_str == NULL)
-        return NULL;
+	new_str = malloc(sizeof(char) * (strlen(str) + 1));
+	if (new_str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
 
-    new_node = malloc(sizeof(list_t));
-    if (new_node == NULL)
-    {
-        free(new_str);
-        return NULL;
-    }
+	for (i = 0; str[i]; i++)
+		new_str[i] = str[i];
+	new_str[i] = '\0';
 
-    memcpy(new_str, str, str_len);
-    new_str[str_len] = '\0';
+	new_node->str = new_str;
+	new_node->len = i;
+	new_node->next = *head;
 
-    new_node->str = new_str;
-    new_node->len = str_len;
-    new_node->next = *head;
+	*head = new_node;
 
-    *head = new_node;
-
-    return new_node;
+	return (new_node);
 }
