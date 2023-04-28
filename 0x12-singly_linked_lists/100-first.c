d01bcf62fee4a9bb13193540d2d85e44ee13c830
mohamed-ifkirn <moifkirn1@gmail.com>
2023-04-28 13:11:14 -0700

    100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+void print(void);
+
+int main(void)
+{
+printf("Hello World!\n");
+	return (0);
+}
+
+void print(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
