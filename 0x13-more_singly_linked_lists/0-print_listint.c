ed1abbf4fe571febd460deb62c84b59b06a2a6d1
mohamed-ifkirn <moifkirn1@gmail.com>
2023-04-29 15:24:57 -0700

    -print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -7,22 +7,21 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * 
+ *
  */
-
 size_t print_listint(const listint_t *h)
 {
-int count = 0;
+	int count = 0;
 
-        if (h != NULL)
-        {
-                while (h)
-                {
-                        printf("%d\n", h->n);
-                        h = h->next;
-                        count++;
-                }
-        }
+	if (h != NULL)
+	{
+	while (h)
+	{
+	printf("%d\n", h->n);
+	h = h->next;
+	count++;
+	}
+}
 
-        return (count);
+	return (count);
 }
